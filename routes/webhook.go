fd1a41ac7476a8c2bf30037ab3435aa543a924c4
Jon McIntosh <jon@designbycosmic.com>
2016-01-06 14:48:26 -0800

    Adding in asana/slack communication

--- routes/webhook.go
+++ routes/webhook.go
@@ -1,23 +1,20 @@
 package routes
 
 import (
-	"github.com/gorilla/context"
-	// "github.com/nlopes/slack"
 	"fmt"
+	"github.com/bluele/slack"
+	"github.com/gorilla/context"
 	"net/http"
 )
 
+const (
+	USER_JON = "U039YMRF8"
+)
+
 func RecieveWebhook(w http.ResponseWriter, r *http.Request) {
-	// api := slack.New(fmt.Sprintf("%v", context.Get(r, "slack")))
+	api := slack.New(fmt.Sprintf("%v", context.Get(r, "slack")))
 
-	// groups, err := api.GetGroups(false)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// for _, group := range groups {
-	// 	fmt.Printf("ID: %s, Name: %s\n", group.ID, group.Name)
-	// }
+	api.ChatPostMessage(USER_JON, "Something happened on Asana!", nil)
 
 	w.Header().Set("X-Hook-Secret", fmt.Sprintf("%v", context.Get(r, "slack-secret")))
 	Respond(http.StatusOK, "Done.", w)
