f0909fd69dd3942979f8939c1b8334edc95ac4b1
Jon McIntosh <jon@designbycosmic.com>
2016-01-06 14:34:12 -0800

    Fixing port

--- config/config.go
+++ config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 type Config struct {
 	Address string
@@ -12,7 +15,7 @@ var Configuration *Config
 func ParseConfig() {
 	Configuration = &Config{}
 
-	Configuration.Address = os.Getenv("PORT")
+	Configuration.Address = fmt.Sprintf(":%v", os.Getenv("PORT"))
 	Configuration.APIKey = "xoxp-2182767778-3338739518-17735596101-6d474c4071"
 
 	return
