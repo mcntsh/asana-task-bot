9408db84c0d85aef2747b829e9616391e2d7c2d6
Jon McIntosh <jon@designbycosmic.com>
2016-01-06 14:18:34 -0800

    Go web server runs on dynamic port

--- config/config.go
+++ config/config.go
@@ -1,5 +1,7 @@
 package config
 
+import "os"
+
 type Config struct {
 	Address string
 	APIKey  string
@@ -10,7 +12,7 @@ var Configuration *Config
 func ParseConfig() {
 	Configuration = &Config{}
 
-	Configuration.Address = ":80"
+	Configuration.Address = os.Getenv("PORT")
 	Configuration.APIKey = "xoxp-2182767778-3338739518-17735596101-6d474c4071"
 
 	return
